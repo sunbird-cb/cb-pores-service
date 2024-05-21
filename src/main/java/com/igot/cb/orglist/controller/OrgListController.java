8e7ddeb9a62f85df02e1cc1dd63e88e0f9d808f8
pathiknandi <pathik.nandi@tarento.com>
2024-05-21 10:34:44 +0530

    second commit for changes path url add bookmark

--- src/main/java/com/igot/cb/orglist/controller/OrgListController.java
+++ src/main/java/com/igot/cb/orglist/controller/OrgListController.java
@@ -10,7 +10,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
-@RequestMapping("/org")
+@RequestMapping("/org/bookmark")
 public class OrgListController {
     @Autowired
     private OrgListService orgListService;
