848feff7f9632da2e6c067234acab2a4da82bd88
pathiknandi <pathik.nandi@tarento.com>
2024-05-22 10:35:09 +0530

    removed orglist packege

--- src/main/java/com/igot/cb/orglist/repository/OrgListRepository.java
+++
@@ -1,8 +0,0 @@
-package com.igot.cb.orglist.repository;
-
-import com.igot.cb.orglist.entity.OrgListEntity;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface OrgListRepository extends JpaRepository<OrgListEntity, String> {
-}
-
