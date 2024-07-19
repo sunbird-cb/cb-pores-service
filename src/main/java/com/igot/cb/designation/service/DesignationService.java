4eca3a0d8a734944adbf8d0367e9c62eabdb1106
anilkumarkammalapalli <121931293+anilkumarkammalapalli@users.noreply.github.com>
2024-07-19 11:28:59 +0530

    Update DesignationService.java

--- src/main/java/com/igot/cb/designation/service/DesignationService.java
+++ src/main/java/com/igot/cb/designation/service/DesignationService.java
@@ -3,7 +3,9 @@ package com.igot.cb.designation.service;
 import com.fasterxml.jackson.databind.JsonNode;
 import com.igot.cb.pores.dto.CustomResponse;
 import com.igot.cb.pores.util.ApiResponse;
+
 import com.igot.cb.pores.elasticsearch.dto.SearchCriteria;
+
 import org.springframework.web.multipart.MultipartFile;
 
 public interface DesignationService {
